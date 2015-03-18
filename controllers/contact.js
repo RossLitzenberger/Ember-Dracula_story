3aa55caf5e111e7bd84fc043f8fff0301e6d067f
RossLitz <codingclasses.049@gmail.com>
2015-03-18 17:29:53 -0500

    reflog

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,10 +1,9 @@
 Blogger.ContactController = Ember.Controller.extend({
-	messageSent: false,
-	actions:{
-		sendMessage: function(){
-			var message = prompt('send the message');
-			this.set('messageSent', true);
-			this.set('confirmationNumber', Math.round(Math.random() * 100000));
-		}
-	}
+  actions: {
+    sendMessage: function() {
+      var message = prompt('Type your message here:');
+      this.set('confirmationNumber', Math.round(Math.random() * 100000));
+      this.set('messageSent', true);
+    }
+  }
 });
--- router.js
+++ router.js
@@ -1,9 +1,8 @@
-Blogger.Router.map(function(){
-	this.resource('posts', {path: '/'});
-	this.resource('about');
-	this.resource('contact', function(){
-		this.resource('phone');
-		this.resource('email');
-	});
+Blogger.Router.map(function() {
+  this.resource('posts', {path: '/'});
+  this.resource('about');
+  this.resource('contact', function() {
+    this.resource('email');
+    this.resource('phone');
+  });
 });
-		
