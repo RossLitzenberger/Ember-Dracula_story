974655477bf76ea6591a3c3f30e94e6a3fe733c0
RossLitz <codingclasses.049@gmail.com>
2015-03-19 20:31:09 -0500

    create comment to show up on each post

--- router.js
+++ router.js
@@ -1,6 +1,9 @@
 Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
-  this.resource('post', {path: '/posts/:post_id'});
+  this.resource('post', {path: '/posts/:post_id'}, function(){
+  	this.resource('new-comment');
+  });
+
   this.resource('new-post');
   this.resource('about');
   this.resource('recent-comments');
